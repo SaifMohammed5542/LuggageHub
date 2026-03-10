a1dbcca22bfa169aefac2653278d4c070cf23d30
Saif Mohammed <saifmohammed5542@gmail.com>
2026-03-10 19:35:46 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/locations/ezymart-660-bourke-st',
+        destination: '/locations/660-bourke-st',
+        permanent: true,
+      },
+      {
+        source: '/locations/ezymart-southern-cross-station',
+        destination: '/locations/southern-cross-station',
+        permanent: true,
+      },
+      {
+        source: '/locations/ezymart-queen-street',
+        destination: '/locations/341-queen-street',
+        permanent: true,
+      },
+      {
+        source: '/locations/luggage-terminal-520-bourke-street-cbd',
+        destination: '/locations/520-bourke-st',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
